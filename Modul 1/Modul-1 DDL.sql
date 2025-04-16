4df9a6a9cac8b070cf1ffdbd30df159c6f7071b5
Meyesss <146051966+Meyesss@users.noreply.github.com>
2025-04-16 09:42:34 +0700

    Update Modul-1 DDL.sql

--- Modul 1/Modul-1 DDL.sql
+++ Modul 1/Modul-1 DDL.sql
@@ -1,5 +1,5 @@
--- Nama : Umaynabila
--- NIM  : 23241056
+-- Nama : Ika Damayanti
+-- NIM  : 23241059
 -- Kelas: B
 
 -- 1. Membuat database
@@ -51,7 +51,7 @@ VALUES
 
 -- 8. Mengisi data contoh pada tabel 'transaksi'
 INSERT INTO transaksi (tgl_jual, id_produk, member_id, nama_produk, harga, qty)
-VALUES ('ID-01', 'webmaster', 'Admin', '8000-01-01', 15);
+VALUES ('ID-01', 'webmaster', 'Admin', '2024-10-10', 10);
 
 -- mengakses isi data pada tabel member
 SELECT * FROM member;
